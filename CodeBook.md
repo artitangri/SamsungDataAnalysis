e6c3b2025a0aca7ee011a4b04256eca2441cf4f5
Arti Tangri <artitangri@yahoo.com>
2015-06-17 23:36:29 -0700

    Added more variables
    Initial files had just 6 variables measured. Added 60 more that were
    missed earlier.

---
+++ CodeBook.md
@@ -0,0 +1,195 @@
+1. Group.1 
+This is data for activities that the subject performs for which the measurement has been taken.
+2. Group.2 
+This is data the subject on which the activity is performed for measurement to be taken.
+3. tBodyAcc-mean()-X 
+Time Domain Body Accelerometer Value Mean along X axis
+
+4. tBodyAcc-mean()-Y 
+Time Domain Body Accelerometer Value Mean along Y axis
+
+5. tBodyAcc-mean()-Z 
+Time Domain Body Accelerometer Value Mean along Z axis
+
+6. tBodyAcc-std()-X 
+Time Domain Body Accelerometer Value Standard Deviation along X axis
+
+7. tBodyAcc-std()-Y 
+Time Domain Body Accelerometer Value Standard Deviation along Y axis
+
+8. tBodyAcc-std()-Z 
+Time Domain Body Accelerometer Value Standard Deviation along Z axis
+
+9. tGravityAcc-mean()-X 
+Time Domain Gravity Accelerometer Value Mean along X axis
+
+10 tGravityAcc-mean()-Y 
+Time Domain Gravity Accelerometer Value Mean along Y axis
+
+11. tGravityAcc-mean()-Z 
+Time Domain Gravity Accelerometer Value Mean along Z axis
+
+12. tGravityAcc-std()-X 
+Time Domain Gravity Accelerometer Value Standard Deviation along X axis
+
+13. tGravityAcc-std()-Y 
+Time Domain Gravity Accelerometer Value Standard Deviation along Y axis
+
+14. tGravityAcc-std()-Z 
+Time Domain Gravity Accelerometer Value Standard Deviation along Z axis
+
+15. tBodyAccJerk-mean()-X 
+Time Domain Body Linear Accelerometer Value Mean along X axis
+
+16. tBodyAccJerk-mean()-Y 
+Time Domain Body Linear Accelerometer Value Mean along Y axis
+
+17. tBodyAccJerk-mean()-Z 
+Time Domain Body Linear Accelerometer Value Mean along Z axis
+
+18. tBodyAccJerk-std()-X 
+Time Domain Body Linear Accelerometer Value Standard Deviation along X axis
+
+19. tBodyAccJerk-std()-Y 
+Time Domain Body Linear Accelerometer Value Standard Deviation along Y axis
+
+20. tBodyAccJerk-std()-Z 
+Time Domain Body Linear Accelerometer Value Standard Deviation along Z axis
+
+21. tBodyGyro-mean()-X 
+Time Domain Body Gyrosope Value Mean along X axis
+
+22. tBodyGyro-mean()-Y 
+Time Domain Body Gyrosope Value Mean along Y axis
+
+23. tBodyGyro-mean()-Z 
+Time Domain Body Gyrosope Value Mean along Z axis
+
+24. tBodyGyro-std()-X 
+Time Domain Body Gyrosope Value Standard Deviation along X axis
+
+25. tBodyGyro-std()-Y 
+Time Domain Body Gyrosope Value Standard Deviation along Y axis
+
+26. tBodyGyro-std()-Z 
+Time Domain Body Gyrosope Value Standard Deviation along Z axis
+
+27. tBodyGyroJerk-mean()-X 
+Time Domain Body Angular Gyrosope Value Mean along X axis
+
+28. tBodyGyroJerk-mean()-Y 
+Time Domain Body Angular Gyrosope Value Mean along Y axis
+
+29. tBodyGyroJerk-mean()-Z 
+Time Domain Body Angular Gyrosope Value Mean along Z axis
+
+30. tBodyGyroJerk-std()-X 
+Time Domain Body Angular Gyrosope Value Standard Deviation along X axis
+
+31. tBodyGyroJerk-std()-Y 
+Time Domain Body Angular Gyrosope Value Standard Deviation along Y axis
+
+32. tBodyGyroJerk-std()-Z 
+Time Domain Body Angular Gyrosope Value Standard Deviation along Z axis
+
+33. tBodyAccMag-mean() 
+Time Domain Body Magnitude (calculated using Euclidean norm) Accelerometer Value Mean 
+
+34. tBodyAccMag-std() 
+Time Domain Body Magnitude (calculated using Euclidean norm) Accelerometer Value Standard Deviation
+
+35. tGravityAccMag-mean() 
+Time Domain Gravity Magnitude (calculated using Euclidean norm) Accelerometer Value Mean
+
+36. tGravityAccMag-std() 
+Time Domain Gravity Magnitude (calculated using Euclidean norm) Accelerometer Value Standard Deviation
+
+37. tBodyAccJerkMag-mean() 
+Time Domain Body Linear Magnitude (calculated using Euclidean norm) Accelerometer Value Mean 
+
+38. tBodyAccJerkMag-std() 
+Time Domain Body Linear Magnitude (calculated using Euclidean norm) Accelerometer Value Standard Deviation
+
+39. tBodyGyroMag-mean() 
+Time Domain Body Magnitude (calculated using Euclidean norm) Gyrosope Value Mean
+
+40. tBodyGyroMag-std() 
+Time Domain Body Magnitude (calculated using Euclidean norm) Gyrosope Value Standard Deviation 
+
+41. tBodyGyroJerkMag-mean() 
+Time Domain Body Angular Magnitude (calculated using Euclidean norm) Gyrosope Value Mean
+
+42. tBodyGyroJerkMag-std() 
+Time Domain Body Angular Magnitude (calculated using Euclidean norm) Gyrosope Value Standard Deviation
+
+43. fBodyAcc-mean()-X 
+Frequency Domain Body Accelerometer Value Mean along X axis
+
+44. fBodyAcc-mean()-Y 
+Frequency Domain Body Accelerometer Value Mean along Y axis
+
+45. fBodyAcc-mean()-Z 
+Frequency Domain Body Accelerometer Value Mean along Z axis
+
+46. fBodyAcc-std()-X 
+Frequency Domain Body Accelerometer Value Standard Deviation along X axis
+
+47. fBodyAcc-std()-Y 
+Frequency Domain Body Accelerometer Value Standard Deviation along Y axis
+
+48. fBodyAcc-std()-Z 
+Frequency Domain Body Accelerometer Value Standard Deviation along Z axis
+
+49. fBodyAccJerk-mean()-X 
+Frequency Domain Body Linear Accelerometer Value Mean along X axis
+
+50. fBodyAccJerk-mean()-Y 
+Frequency Domain Body Linear Accelerometer Value Mean along Y axis
+
+51. fBodyAccJerk-mean()-Z 
+Frequency Domain Body Linear Accelerometer Value Mean along Z axis
+
+52. fBodyAccJerk-std()-X 
+Frequency Domain Body Linear Accelerometer Value Standard Deviation along X axis
+
+53. fBodyAccJerk-std()-Y 
+Frequency Domain Body Linear Accelerometer Value Standard Deviation along Y axis
+
+54. fBodyAccJerk-std()-Z 
+Frequency Domain Body Linear Accelerometer Value Standard Deviation along Z axis
+
+55. fBodyGyro-mean()-X 
+Frequency Domain Body Gyrosope Value Mean along X axis
+
+56. fBodyGyro-mean()-Y 
+Frequency Domain Body Gyrosope Value Mean along Y axis
+
+57. fBodyGyro-mean()-Z 
+Frequency Domain Body Gyrosope Value Mean along Z axis
+
+58. fBodyGyro-std()-X 
+Frequency Domain Body Gyrosope Value Standard Deviation along X axis
+
+59. fBodyGyro-std()-Y 
+Frequency Domain Body Gyrosope Value Standard Deviation along Y axis
+
+60. fBodyGyro-std()-Z 
+Frequency Domain Body Gyrosope Value Standard Deviation along Z axis
+
+61. fBodyAccMag-mean() 
+Frequency Domain Body Magnitude (calculated using Euclidean norm) Accelerometer Value Mean
+
+62. fBodyAccMag-std() 
+Frequency Domain Body Magnitude (calculated using Euclidean norm) Accelerometer Value Standard Deviation
+
+63. fBodyBodyGyroMag-mean() 
+Frequency Domain Body Magnitude (calculated using Euclidean norm) Gyroscope Value Mean
+
+64. fBodyBodyGyroMag-std() 
+Frequency Domain Body Magnitude (calculated using Euclidean norm) Gyroscope Value Standard Deviation
+
+65. fBodyBodyGyroJerkMag-mean() 
+Frequency Domain Body Angular Magnitude (calculated using Euclidean norm) Gyroscope Value Mean
+
+66. fBodyBodyGyroJerkMag-std() 
+Frequency Domain Body Angular Magnitude (calculated using Euclidean norm) Gyroscope Value Standard Deviation
--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,8 @@
-## Step 1 Merge training and test data
+##################################
+########## Step 1 ################
+## Merge training and test data ##
+##################################
+
 ## Read the training data from the files.
 ## Measurements
 X_trainDF <- read.table("train/X_train.txt")
@@ -38,24 +42,105 @@ test_DF$set <- "test"
 ## Merge the data using merge command setting all=TRUE to get all data from both DF
 merged_DF <- merge(train_DF, test_DF, all=TRUE)
 
-## Step 2 Extract mean and SD data which are the first 6 columns in the training and test data files.
-Mean_SD_DF <- merged_DF[,c("Activity", "Subject", "V1", "V2", "V3", "V4", "V5", "V6", "set")]
+##################################
+########### Step 2 ###############
+#### Extract mean and SD data ####
+##################################
+Mean_SD_DF <- merged_DF[,c("Activity", "Subject", "V1", "V2", "V3", "V4", "V5", "V6", 
+                           "V41", "V42", "V43", "V44", "V45", "V46", 
+                           "V81", "V82", "V83", "V84", "V85", "V86",
+                           "V121", "V122", "V123", "V124", "V125", "V126",
+                           "V161", "V162", "V163", "V164", "V165", "V166",
+                           "V201", "V202", "V214", "V215", "V227", "V228",
+                           "V240", "V241", "V253", "V254",
+                           "V266", "V267", "V268", "V269", "V270", "V271",
+                           "V345", "V346", "V347", "V348", "V349", "V350",
+                           "V424", "V425", "V426", "V427", "V428", "V429",
+                           "V503", "V504", "V529", "V530", "V542", "V543",
+                           "set")]
 
-## Step 3 Use descriptive activity names
+##################################
+########## Step 3 ################
+## Use descriptive activity names
+##################################
 Mean_SD_DF[, 1] <- activity_DF[Mean_SD_DF[,1], 2]
 
-## Step 4 Label the variables in the dataset
+#####################################
+############# Step 4 ################
+## Label the variables in the dataset
+#####################################
+
 ## Get variable names from features.txt
 features <- read.table("features.txt")
 
-## First 6 variables in the features are mean and SD, assign those values as column names to the final dataset
-colnames(Mean_SD_DF)[3] <- features[1,2]
-colnames(Mean_SD_DF)[4] <- features[2,2]
-colnames(Mean_SD_DF)[5] <- features[3,2]
-colnames(Mean_SD_DF)[6] <- features[4,2]
-colnames(Mean_SD_DF)[7] <- features[5,2]
-colnames(Mean_SD_DF)[8] <- features[6,2]
+## Assign column names from features DF to the final dataset
+colnames(Mean_SD_DF)[3] <- as.character(features[1,2])
+colnames(Mean_SD_DF)[4] <- as.character(features[2,2])
+colnames(Mean_SD_DF)[5] <- as.character(features[3,2])
+colnames(Mean_SD_DF)[6] <- as.character(features[4,2])
+colnames(Mean_SD_DF)[7] <- as.character(features[5,2])
+colnames(Mean_SD_DF)[8] <- as.character(features[6,2])
+colnames(Mean_SD_DF)[9] <- as.character(features[41,2])
+colnames(Mean_SD_DF)[10] <- as.character(features[42,2])
+colnames(Mean_SD_DF)[11] <- as.character(features[43,2])
+colnames(Mean_SD_DF)[12] <- as.character(features[44,2])
+colnames(Mean_SD_DF)[13] <- as.character(features[45,2])
+colnames(Mean_SD_DF)[14] <- as.character(features[46,2])
+colnames(Mean_SD_DF)[15] <- as.character(features[81,2])
+colnames(Mean_SD_DF)[16] <- as.character(features[82,2])
+colnames(Mean_SD_DF)[17] <- as.character(features[83,2])
+colnames(Mean_SD_DF)[18] <- as.character(features[84,2])
+colnames(Mean_SD_DF)[19] <- as.character(features[85,2])
+colnames(Mean_SD_DF)[20] <- as.character(features[86,2])
+colnames(Mean_SD_DF)[21] <- as.character(features[121,2])
+colnames(Mean_SD_DF)[22] <- as.character(features[122,2])
+colnames(Mean_SD_DF)[23] <- as.character(features[123,2])
+colnames(Mean_SD_DF)[24] <- as.character(features[124,2])
+colnames(Mean_SD_DF)[25] <- as.character(features[125,2])
+colnames(Mean_SD_DF)[26] <- as.character(features[126,2])
+colnames(Mean_SD_DF)[27] <- as.character(features[161,2])
+colnames(Mean_SD_DF)[28] <- as.character(features[162,2])
+colnames(Mean_SD_DF)[29] <- as.character(features[163,2])
+colnames(Mean_SD_DF)[30] <- as.character(features[164,2])
+colnames(Mean_SD_DF)[31] <- as.character(features[165,2])
+colnames(Mean_SD_DF)[32] <- as.character(features[166,2])
+colnames(Mean_SD_DF)[33] <- as.character(features[201,2])
+colnames(Mean_SD_DF)[34] <- as.character(features[202,2])
+colnames(Mean_SD_DF)[35] <- as.character(features[214,2])
+colnames(Mean_SD_DF)[36] <- as.character(features[215,2])
+colnames(Mean_SD_DF)[37] <- as.character(features[227,2])
+colnames(Mean_SD_DF)[38] <- as.character(features[228,2])
+colnames(Mean_SD_DF)[39] <- as.character(features[240,2])
+colnames(Mean_SD_DF)[40] <- as.character(features[241,2])
+colnames(Mean_SD_DF)[41] <- as.character(features[253,2])
+colnames(Mean_SD_DF)[42] <- as.character(features[254,2])
+colnames(Mean_SD_DF)[43] <- as.character(features[266,2])
+colnames(Mean_SD_DF)[44] <- as.character(features[267,2])
+colnames(Mean_SD_DF)[45] <- as.character(features[268,2])
+colnames(Mean_SD_DF)[46] <- as.character(features[269,2])
+colnames(Mean_SD_DF)[47] <- as.character(features[270,2])
+colnames(Mean_SD_DF)[48] <- as.character(features[271,2])
+colnames(Mean_SD_DF)[49] <- as.character(features[345,2])
+colnames(Mean_SD_DF)[50] <- as.character(features[346,2])
+colnames(Mean_SD_DF)[51] <- as.character(features[347,2])
+colnames(Mean_SD_DF)[52] <- as.character(features[348,2])
+colnames(Mean_SD_DF)[53] <- as.character(features[349,2])
+colnames(Mean_SD_DF)[54] <- as.character(features[350,2])
+colnames(Mean_SD_DF)[55] <- as.character(features[424,2])
+colnames(Mean_SD_DF)[56] <- as.character(features[425,2])
+colnames(Mean_SD_DF)[57] <- as.character(features[426,2])
+colnames(Mean_SD_DF)[58] <- as.character(features[427,2])
+colnames(Mean_SD_DF)[59] <- as.character(features[428,2])
+colnames(Mean_SD_DF)[60] <- as.character(features[429,2])
+colnames(Mean_SD_DF)[61] <- as.character(features[503,2])
+colnames(Mean_SD_DF)[62] <- as.character(features[504,2])
+colnames(Mean_SD_DF)[63] <- as.character(features[529,2])
+colnames(Mean_SD_DF)[64] <- as.character(features[530,2])
+colnames(Mean_SD_DF)[65] <- as.character(features[542,2])
+colnames(Mean_SD_DF)[66] <- as.character(features[543,2])
 
-## Step 5 Create a new tidy dataset with average of each variable for each activity and each subject
-Mean_SD_DF.AVG <- aggregate(Mean_SD_DF[, 3:8], list(Mean_SD_DF$Activity, Mean_SD_DF$Subject), mean)
+##################################
+########## Step 5 ################
+## Create a new tidy dataset with average of each variable for each activity and each subject
+Mean_SD_DF.AVG <- aggregate(Mean_SD_DF[, 3:66], list(Mean_SD_DF$Activity, Mean_SD_DF$Subject), mean)
 
